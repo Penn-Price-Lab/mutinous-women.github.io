e86c1cf18461f2a5cac45c75b7fc70d5cac334fd
PaulineCbl <39924703+PaulineCbl@users.noreply.github.com>
2021-11-10 11:55:40 -0500

    another update

--- js/map.js
+++ js/map.js
@@ -1,13 +1,12 @@
 let myMap = L.map('mapid', {rotate:true});
-myMap.setView([29.951065
-, -90.071533], 15);
+myMap.setView([29.961368, -90.057744], 15);
 
-let tilesbg = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
+let tilesbg = L.tileLayer('https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer/tile/{z}/{y}/{x}')
 let tiles = L.tileLayer("https://mapwarper.net/maps/tile/58607/{z}/{x}/{y}.png")
 tilesbg.addTo(myMap)
 tiles.addTo(myMap);
 
-$.getJSON("https://cdn.glitch.com/095c6d41-a3ec-49e6-a0eb-30d2394c0254%2FMutinousWomen(1).geojson?v=1633112697275", function(data){
+$.getJSON("https://cdn.glitch.me/095c6d41-a3ec-49e6-a0eb-30d2394c0254%2FNovember9thGeo.geojson?v=1636507234968", function(data){
   
 // let legend = L.control({position: 'bottomright'});
   
@@ -39,9 +38,44 @@ myMap.setBearing(52)
 
 function addPopUp(feature,layer){
   
-  let content = "Name:" + feature.properties.Name + "<br>" 
-                + "Lot number:" + feature.properties.Lot;
-  layer.bindPopup(content)
+  if (feature.properties.Type=="street"||"person"){
+    
+    
+    
+
+  
+  let content = "<b>"+"Name: " + feature.properties.Name + "<br>" 
+                + "Lot number: " + feature.properties.Lot + "<br>"
+                + "Ship, Passenger number: " + feature.properties.Ship_number + "<br>"
+                + "Birth: "+ feature.properties.Birth + "<br>"
+                + "Death: "+ feature.properties.Death + "<br>"+"</b>"
+                + "<br>"
+                + feature.properties.Bio + "<br>"
+                +"<br>"
+                + "<img width=300 src='"+feature.properties.Image+"'>"
+                + "<br>"
+  
+                 
+ layer.bindPopup(content)   }
+  
+  else if (feature.properties.Type=="public building"){
+  
+  
+    let content = "<b>"+"Name: " + feature.properties.Name + "<br>" 
+                + "Lot number: " + feature.properties.Lot + "<br>"
+                + "Ship, Passenger number: " + feature.properties.Ship_number + "<br>"
+                + "Birth: "+ feature.properties.Birth + "<br>"
+                + "Death: "+ feature.properties.Death + "<br>"+"</b>"
+                + "<br>"
+                + feature.properties.Bio + "<br>"
+                +"<br>"
+                + "<img width=300 src='"+feature.properties.Image+"'>"
+                + "<br>"
+  
+                 
+ layer.bindPopup(content)   }
+  
+  
   
 }
 
@@ -71,16 +105,17 @@ function getColor(feature){
 myMap.on("contextmenu", function (event) {
   document.getElementById("title").innerText = event.latlng.toString();
   console.log("user right-clicked on map coordinates: " + event.latlng.toString());
-  L.marker(event.latlng).addTo(myMap);
+  //L.marker(event.latlng).addTo(myMap);
 });
 
 
 
 function getcolor (feature) {
-  if(feature.properties.type == "person") {return 'blue'
+  if(feature.properties.Type == "person") {return 'blue'
     
   }
-  else if (feature.properties.type == "street") {return 'orange'}
+  else if (feature.properties.Type == "street") {return 'orange'}
+  else if (feature.properties.Type == "public building") {return 'green'}
   else {return 'black'}
 };
 
