6dda0ea8a04ab5964473159ebbe36b6922b343a3
PaulineCbl <39924703+PaulineCbl@users.noreply.github.com>
2021-11-13 15:52:31 -0500

    removed the bracket passage...

--- js/map.js
+++ js/map.js
@@ -1,142 +1,61 @@
-let myMap = L.map('mapid', {rotate:true});
-myMap.setView([29.957588538774026, -90.06292144027744], 15);
+/******************************************************************************
+START Glitch hello-app default styles
 
-let tilesbg = L.tileLayer('https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer/tile/{z}/{y}/{x}')
-let tiles = L.tileLayer("https://mapwarper.net/maps/tile/58607/{z}/{x}/{y}.png")
-tilesbg.addTo(myMap)
-tiles.addTo(myMap);
-
-$.getJSON("https://cdn.glitch.me/095c6d41-a3ec-49e6-a0eb-30d2394c0254%2FMutinousWomen5.geojson?v=1636589906502", function(data){
-  
-// let legend = L.control({position: 'bottomright'});
-  
-// legend.addTo(myMap);
-  
-  let rainCheckSites = L.geoJson(data, {
-    
-    pointToLayer: function(feature, latlng) {
-      return new L.CircleMarker(latlng, {
-        radius:2,
-        color: getcolor(feature)
-      });
-    },
-    onEachFeature: addPopUp
-    
-    
-  });
-  
-  
-  
-  rainCheckSites.addTo(myMap);
-  
-myMap.setBearing(52)
-  
-})
+The styles in this section do some minimal CSS resets, set default fonts and 
+colors, and handle the layout for our footer and "Remix on Glitch" button. If
+you're new to CSS they may seem a little complicated, but you can scroll down
+to this section's matching END comment to see page-specific styles.
+******************************************************************************/
 
 
+/* 
+  The style rules specify elements by type and by attributes such as class and ID
+  Each section indicates an element or elements, then lists the style properties to apply
+  See if you can cross-reference the rules in this file with the elements in index.html
+*/
 
+/* Our default values set as CSS variables */
+:root {
+  --color-bg: #ffccff;
 
-function addPopUp(feature,layer){
-  
-  if (feature.properties.Type=="street"||feature.properties.Type=="person"){
-    
-    
-    
+}
 
-  
-  let content = "<b>"+"Name: " + feature.properties.Name + "<br>" 
-                + "Lot number: " + feature.properties.Lot + "<br>"
-                + "Ship, Passenger number: " + feature.properties.Ship_number + "<br>"
-                + "Birth: "+ feature.properties.Birth + "<br>"
-                + "Death: "+ feature.properties.Death + "<br>"+"</b>"
-                + "<br>"
-                + feature.properties.Bio + "<br>"
-                +"<br>"
-                + "<img width=300 src='"+feature.properties.Image+"'>"
-                + "<br>"
-  
-                 
- layer.bindPopup(content)   }
-  
-  else if (feature.properties.Type=="public building"){
-  
-  
-    let content = "<b>"
-                + "<b>"+ feature.properties.Name + "<br>" 
-                +"<br>"
-                + feature.properties.Bio + "<br>"
-                +"<br>"
-                + "<img width=300 src='"+feature.properties.Image+"'>"
-                + "<br>"
-  
-                 
- layer.bindPopup(content)   }
-  
-   else if (feature.properties.Type=="other "){
- 
- let content = "<b>"+"Name: " + feature.properties.Name + "<br>" 
-                + "Lot number: " + feature.properties.Lot + "<br>"
-                + "Ship, Passenger number: " + feature.properties.Ship_number + "<br>"
-                + "Birth: "+ feature.properties.Birth + "<br>"
-                + "Death: "+ feature.properties.Death + "<br>"+"</b>"
-                + "<br>"
-                + feature.properties.Bio + "<br>"
-                +"<br>"
-                + "<img width=300 src='"+feature.properties.Image+"'>"
-                + "<br>"
-  
-      
-               
-  
-                 
- layer.bindPopup(content)  
-    
-    
-  }
-  
-  
+/* Import fonts */
+@font-face {
+  font-family: HK Grotesk;
+  src: url("https://cdn.glitch.me/095c6d41-a3ec-49e6-a0eb-30d2394c0254%2Fnov13.geojson?v=1636836666566")
+    format("opentype");
+}
+@font-face {
+  font-family: HK Grotesk;
+  font-weight: bold;
+  src: url("https://cdn.glitch.com/605e2a51-d45f-4d87-a285-9410ad350515%2FHKGrotesk-Bold.otf?v=1603136323437")
+    format("opentype");
 }
 
 
+body {
+  font-family: Verdana;
+  background-color: var(--color-bg);
+}
 
-function getColor(feature){
-  switch (feature.properties.TOOLS_INSTALLED) {
-            case 'depaving':
-              return  'orange';
-            case 'downspout planter':
-              return 'green';
-            case 'Downspout planter':
-              return 'green';
-            case 'rain garden':
-              return 'blue';
-            case 'rain barrel':
-              return 'purple';
-            case 'Rain Barrel':
-              return 'purple;'
-            case 'permeable pavers':
-              return 'pink';
-            case 'Permeable pavers':
-              return 'pink';
-            default:
-              return 'black';
-          }
-        }
+/* Page structure */
+.title {
+  color: #2800FF;
+  font-family: verdana;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 60px;
+  line-height: 105%;
+  margin: 0;
+}
 
-myMap.on("contextmenu", function (event) {
-  document.getElementById("title").innerText = event.latlng.toString();
-  console.log("user right-clicked on map coordinates: " + event.latlng.toString());
-  //L.marker(event.latlng).addTo(myMap);
-});
+.container {
+  margin:1em;
+}
 
-
-
-function getcolor (feature) {
-  if(feature.properties.Type == "person") {return '#1F618D'
-    
-  }
-  else if (feature.properties.Type == "street") {return '#AF601A'}
-  else if (feature.properties.Type == "public building") {return '#1D8348'}
-  else {return 'black'}
-};
-
+#mapid {
+  height:600px;
+  
+}
 
